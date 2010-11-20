34ba87063eb5606a39fbc05898fbe5beab225d4b
Yonatan Offek <my.name@gmail.com>
2010-11-20 02:10:08 +0200

    Better UI.

--- drupal/sites/default/modules/las3rfl_code/editor.tpl.php
+++ drupal/sites/default/modules/las3rfl_code/editor.tpl.php
@@ -5,25 +5,30 @@ drupal_add_js($module_path .'/swfobject/swfobject.js');
 drupal_add_js($module_path .'/nomacs.js');
 drupal_add_css($module_path .'/nomacs.css');
 ?>
-<table cellspacing=0 cellpadding=0>
+<table id="editor-container" cellspacing=0 cellpadding=0>
   <tr>
     <td id="editor">
       <div id="editorSWF"></div>
     </td>
     <td id="evaluator">
-      <div id="evaluatorSWF"></div>
+      <div id="evaluator-container">
+	<div id="evaluatorSWF"></div>
+      </div>
 
       <!-- fake content-bar for aesthetic purposes -->
       <div class="content-bar">
 	<div class="left">
 	  <ul class="tabs primary">
-	    <li><a id="resetBtn" href="#">Reset eval SWF</a></li>
-	    <li><a href="#">Fullscreen</a></li>
+	    <li>
+	      <a id="resetBtn" href="#">Reset</a>
+	    </li>
+	    <li id="fullscreen">
+	      <a id="fullscreenBtn" href="#">Fullscreen</a>
+	    </li>
 	  </ul>
 	</div>
       </div>
-      
-      <p>In the event of a crash; reload the page and press Alt-R in the editor to recover the last version.</p>
+      <p>In the event of a crash; reload the page and press Alt-R in the editor to recover the last version. Press Ctrl-H for help.</p>
     </td>
   </tr>
 </table>
--- drupal/sites/default/modules/las3rfl_code/nomacs.js
+++ drupal/sites/default/modules/las3rfl_code/nomacs.js
@@ -10,8 +10,56 @@ function resetEvalSWF() {
 }
 
 $(function() {
-    $("#resetBtn").click(resetEvalSWF);
+    function init() {
+      $("#fullscreenBtn").click(enterFullscreen);
+      enterFullscreen();
+    }
 
+    function enterFullscreen() {
+      var saved = [];
+      function setAndSave(selector, property, value) {
+	saved.push({selector: selector, property: property, value: $(selector).css(property)});
+	$(selector).css(property, value);
+      }
+
+      function exitFullscreen() {
+	while(saved.length) {
+	  var x = saved.pop();
+	  $(x.selector).css(x.property, x.value);
+	}
+	$("#fullscreenBtn").unbind("click").click(enterFullscreen).text("Fullscreen");
+	$(window).unbind("resize");
+	$("#editor-container").css("height", "auto");
+      }
+
+      setAndSave("#main", "padding", "0 5px");
+      setAndSave("#squeeze", "padding", "0");
+      setAndSave(
+	"#header .region-content, #sidebar-wrapper, #breadcrumb .region-content, #featured .region-content, #content-bottom .region-content, #footer .region-content, #top-bar .region-content",
+	"max-width",
+	"1465px");
+      setAndSave(
+	"#header, #featured, #breadcrumb, #sidebar-right, #content-bottom, #footer, #squeeze > h2, #squeeze > .content-bar",
+	"display",
+	"none");
+      if(!$.browser.mozilla) { // causes swfs to reload on firefox
+	setAndSave("body", "overflow-y", "hidden");
+      }
+
+      resize();
+      $(window).resize(resize);
+
+      $("#fullscreenBtn").unbind("click").click(exitFullscreen).text("Exit fullscreen");
+    }
+
+    // Resize handler - sets editor height to window height.
+    function resize() {
+      $("#editor-container").css({height: $(window).height()});
+    }
+
+    init();
     swfobject.embedSWF(modulePath + "Nomacs.swf", "editorSWF", "100%", "100%", "9.0.0", null, {connToken: token});
     resetEvalSWF();
+
+    $("#resetBtn").click(resetEvalSWF);
 });
