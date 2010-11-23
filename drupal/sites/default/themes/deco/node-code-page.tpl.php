1135f64e96ecba67d1dd2743dd49bc1c9b290075
Yonatan Offek <my.name@gmail.com>
2010-11-23 16:50:12 +0200

    Added raw node menu hook and links.

--- drupal/sites/default/themes/deco/node-code-page.tpl.php
+++ drupal/sites/default/themes/deco/node-code-page.tpl.php
@@ -1,6 +1,6 @@
 <div id="node-<?php print $node->nid; ?>" class="node node-page<?php if (!$status) { print ' node-unpublished'; } ?>">
   <div class="node-body">
-    <?php print $picture; ?>
+    <?php $raw_node = node_load($node->nid) ?>
     <div class="node-title clear-block">
     </div>
     
@@ -9,13 +9,13 @@
 	<a href="javascript:void(0);" title="Play" id="play-button"></a>
 	<?php print $node->field_capture[0]['view'] ?>
       </div>
-      <?php if ($submitted): ?>
-      <div class="submitted"><p><?php print $submitted; ?></p></div>
-      <?php endif; ?>
-      <div class="forked-from"><p>Forked from: <?php print $node->field_forked_from[0]['view'] ?></p></div>
-      <?php $raw_node = node_load($node->nid) ?>
+      <div class="submitted"><p>Posted by:<br/><?php print $submitted; ?></p></div>
+      <div class="forked-from"><p>Forked from:<br/><?php print $node->field_forked_from[0]['view'] ?></p></div>
       <div class="clear-block"></div>
-      <pre class="las3r-code"><?php print check_plain($raw_node->body) ?></pre>
+      <div class="code-container">
+	
+	<pre class="las3r-code"><div class="raw-link-container"><a class="raw-code-link" href="/node/<?php print $node->nid?>/raw">[View raw code]</a></div><?php print check_plain($raw_node->body) ?></pre>
+      </div>
     </div>
     <div class="hr"><span></span></div>
 
@@ -29,6 +29,7 @@
       <?php if ($links): ?>
       <div class="links"><?php print $links; ?></div>
       <?php endif; ?>
+
     </div>
   </div>
 </div>
