05d083858b50eb420ec434d0422c57580632787c
Yonatan Offek <my.name@gmail.com>
2010-11-24 22:30:42 +0200

    Conditionally remove subject line from comment template.

--- drupal/sites/default/themes/deco/comment.tpl.php
+++ drupal/sites/default/themes/deco/comment.tpl.php
@@ -1,19 +1,21 @@
 <div class="comment<?php if (isset($comment->status) && $comment->status == COMMENT_NOT_PUBLISHED) print ' comment-unpublished'; print ($zebra ? ' comment-'.$zebra : ''); ?>">
-<?php if ($new != '') { ?><span class="new"><?php print $new; ?></span><?php } ?>
+  <?php if ($new != '') { ?><span class="new"><?php print $new; ?></span><?php } ?>
 
-	<div class="comment-top">
-		<?php print $picture; ?>
-		<h3 class="title"><?php print $title; ?></h3>
-    <?php if ($submitted): ?>
-	    <span class="submitted"><?php print $submitted; ?></span>
-	  <?php endif; ?>
-		</div>
-    <div class="content"><?php print $content; ?></div>
-    <?php if ($signature): ?>
-        <div class="signature">
-          <?php print $signature ?>
-        </div>
+  <div class="comment-top">
+    <?php print $picture; ?>
+    <?php if (variable_get('comment_subject_field_'. $node->type, 1)): ?>
+    <h3 class="title"><?php print $title; ?></h3>
     <?php endif; ?>
-		<div class="hr"><span></span></div>
-    <div class="links"><?php print $links; ?></div>
+    <?php if ($submitted): ?>
+    <span class="submitted"><?php print $submitted; ?></span>
+    <?php endif; ?>
+  </div>
+  <div class="content"><?php print $content; ?></div>
+  <?php if ($signature): ?>
+  <div class="signature">
+    <?php print $signature ?>
+  </div>
+  <?php endif; ?>
+  <div class="hr"><span></span></div>
+  <div class="links"><?php print $links; ?></div>
 </div>
